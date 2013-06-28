d6e9299efb75ae3311ef92fd546c598ad6cee6c2
Yoriz <dave.wilson1@ntlworld.com>
2013-06-28 01:24:43 +0100

    I'm bouncing off the walls

--- src/data/main.py
+++ src/data/main.py
@@ -11,16 +11,34 @@ class BallTest(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.screen = screen
         self.color = (255, 255, 255)
-        self.pos = (WIDTH // 2, HEIGHT // 2)
+        self.pos = [WIDTH // 2, HEIGHT // 2]
         self.radius = 25
         self.width = 0
-        self.vel = (2, 1)
+        self.vel = [2, 1]
         print(self.pos)
-        
-    def update(self):
-        self.pos = (self.pos[0] + self.vel[0], self.pos[1] + self.vel[1])
-        pygame.draw.circle(self.screen, self.color, self.pos, self.radius, self.width)
 
+    def update(self):
+        for index in range(2):
+            self.pos[index] += self.vel[index]
+        if any((self.ballHitLeftWall(), self.ballHitRightWall())):
+            self.vel[0] = -self.vel[0]
+        if any((self.ballHitTopWall(), self.ballHitBottomWall())):
+            self.vel[1] = - self.vel[1]
+        pygame.draw.circle(self.screen, self.color, self.pos, self.radius,
+                           self.width)
+        
+
+    def ballHitLeftWall(self):
+        return self.pos[0] <= self.radius
+    
+    def ballHitRightWall(self):
+        return self.pos[0] >= WIDTH-self.radius
+    
+    def ballHitTopWall(self):
+        return self.pos[1] <= self.radius
+    
+    def ballHitBottomWall(self):
+        return self.pos[1] >= HEIGHT-self.radius
 
 class Control:
     def __init__(self):
@@ -32,10 +50,10 @@ class Control:
         self.gamestate = True
         self.ball = BallTest(self.screen)
         self.mainloop()
-        
+
     def update(self):
         self.ball.update()
-        
+
     def mainloop(self):
         while self.gamestate:
             for event in pygame.event.get():
@@ -43,6 +61,6 @@ class Control:
                     self.gamestate = False
             self.update()
             pygame.display.flip()
-            
+
 if __name__ == '__main__':
     app = Control()
